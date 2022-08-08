6c704f20ddf13fa326ed597e245d813409388096
lilianfontan <lilian.fontan@gmail.com>
2022-08-08 20:20:52 -0300

    Visualização do Exercício

---
+++ exercicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets\css\estilo.css">
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+    
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS &copy <?= date('Y'); ?>
+    </footer>
+        
+        
+</body>
+</html>
